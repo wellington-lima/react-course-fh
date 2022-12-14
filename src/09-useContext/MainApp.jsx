89d6411965b6b18d9787a4210b54c86c5a342b51
Wellington <wlima2santos@gmail.com>
2022-12-14 08:09:38 -0300

    criadas rotas da aplicacao

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,8 +1,21 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage';
+import { AboutPage } from './AboutPage';
+
 export const MainApp = () => {
   return(
     <>
       <h1>MainApp</h1>
       <hr />
+
+      <Routes>
+        <Route path='/' element={ <HomePage /> } />
+        <Route path='about' element={ <AboutPage /> } />
+        <Route path='/login' element={ <LoginPage /> } />
+
+        <Route path='/*' element={ <Navigate to='login' /> } />
+      </Routes>
     </>
   )
 }
